bdaad61350782db7538cb0af0b9e9520a9902bc9
Deepali <deepali.sirsath1998@gmail.com>
2021-05-17 22:24:53 +0530

    add first feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-
+//feature -1
+import Recat from "react";
 function App() {
   return (
     <div className="grid-container">
@@ -9,8 +9,8 @@ function App() {
       <main>
         Product List
       </main>
-     <footer>
-       All right is reserved.
+      <footer>
+        All right is reserved.
      </footer>
     </div>
   );
